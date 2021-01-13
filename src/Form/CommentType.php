b65ef16ea1a397c3dc6b12606a5ed698753dc6de
KevinVanphavong <kevin45van@hotmail.fr>
2021-01-13 23:25:55 +0100

    adding stars instead of number rate

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -17,16 +17,11 @@ class CommentType extends AbstractType
             ->add('commentText',TextType::class)
             ->add('rate', ChoiceType::class, [
                 'choices' => [
-                    50 => 1,
+                    1 => 1,
                     2 => 2,
                     3 => 3,
                     4 => 4,
                     5 => 5,
-                    6 => 6,
-                    7 => 7,
-                    8 => 8,
-                    9 => 9,
-                    10 => 10
                 ]
             ])
         ;
